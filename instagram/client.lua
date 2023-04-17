5f338a3fc599f65c11a34141acd1bfb50dd3738d
vexxy <122513007+vexxy69@users.noreply.github.com>
2023-04-17 15:15:41 +0100

    Add files via upload

---
+++ instagram/client.lua
@@ -0,0 +1,48 @@
+local instaHandle = ''
+
+RegisterCommand('instahandle', function(source, args)
+    local handle = table.concat(args, ' ')
+    if handle ~= '' then
+        TriggerServerEvent('setInstaHandle', handle)
+        instaHandle = handle
+        TriggerEvent('chat:addMessage', {
+            color = {204, 0, 204},
+            multiline = true,
+            args = {'[Instagram]', 'Your Instagram handle has been set to: ' .. handle}
+        })
+    else
+        TriggerEvent('chat:addMessage', {
+            color = {204, 0, 204},
+            multiline = true,
+            args = {'[Instagram]', 'Usage: /instahandle [handle]'}
+        })
+    end
+end, false)
+
+RegisterCommand('inst', function(source, args)
+    local message = table.concat(args, ' ')
+    if message ~= '' then
+        TriggerServerEvent('sendInstaPost', instaHandle, message)
+    else
+        TriggerEvent('chat:addMessage', {
+            color = {204, 0, 204},
+            multiline = true,
+            args = {'[Instagram]', 'Usage: /inst [message]'}
+        })
+    end
+end, false)
+
+RegisterNetEvent('setInstaHandle')
+AddEventHandler('setInstaHandle', function(handle)
+    instaHandle = handle
+end)
+
+RegisterNetEvent('receiveInstaPost')
+AddEventHandler('receiveInstaPost', function(handle, message)
+    local name = ('[Instagram] | %s'):format(handle)
+    TriggerEvent('chat:addMessage', {
+        color = {204, 0, 204},
+        multiline = true,
+        args = {name, message}
+    })
+end)
---
+++ instagram/server.lua
@@ -0,0 +1,19 @@
+local instaHandle = ''
+
+RegisterServerEvent('setInstaHandle')
+AddEventHandler('setInstaHandle', function(handle)
+    instaHandle = handle
+end)
+
+RegisterServerEvent('sendInstaPost')
+AddEventHandler('sendInstaPost', function(handle, message)
+    TriggerClientEvent('receiveInstaPost', -1, handle, message)
+end)
+
+AddEventHandler('playerDropped', function()
+    local source = source
+    if GetPlayerName(source) == instaHandle then
+        instaHandle = ''
+        TriggerClientEvent('setInstaHandle', -1, '')
+    end
+end)
